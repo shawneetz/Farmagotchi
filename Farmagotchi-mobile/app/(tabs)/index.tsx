faa6adb8b6b5874fd25e5d6550e7786b8c191bde
klnwlks <kalinawlukasaombebis@gmail.com>
2026-02-26 22:17:10 +0800

    migrate to expo router

---
+++ Farmagotchi-mobile/app/(tabs)/index.tsx
@@ -0,0 +1,5 @@
+import MainDashboardMockup from '../../components/MainDashboardMockup';
+
+export default function DashboardScreen() {
+  return <MainDashboardMockup />;
+}
---
+++ Farmagotchi-mobile/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import '../global.css';
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="dark" />
+    </SafeAreaProvider>
+  );
+}
